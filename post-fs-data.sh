b9cdc727cb8ad8eb707b7e73b997cb4b84fc9181
Рабах Аззам <64314390+RabahX@users.noreply.github.com>
2023-11-20 00:32:53 +0100

    YT-ReVanced-Module

--- post-fs-data.sh
+++ post-fs-data.sh
@@ -2,22 +2,14 @@
 
 # Module directory and name
 MODDIR="${0%/*}"
-NVBASE="/data/adb"
+MAIN_MODULE_PROP="/data/adb/modules/$(basename "$(dirname "$0")")/module.prop"
 
-# File paths
-MAIN_MODULE_PROP="$NVBASE/modules/$(basename "$(dirname "$0")")/module.prop"
-TMP_MODULE_PROP="$NVBASE/yt-revanced/.tmp/yt-revanced.prop"
+# Copy module.prop to the temporary location and modify the description
+FAILED_STATUS=$(sed -E 's/^description=(\[.*][[:space:]]*)?/description=[⛔ The module is non-functioning] /g' "$MODDIR/module.prop")
+echo -n "$FAILED_STATUS" > "$MAIN_MODULE_PROP"
 
-# Copy module.prop to the temporary location
-cp -af "$MODDIR/module.prop" "$TMP_MODULE_PROP"
+# Remove the status file and temporary file
+rm -rf "/data/adb/yt-revanced/.tmp/status"
 
-# Modify the description in the temporary file
-sed -Ei 's/^description=(\[.*][[:space:]]*)?/description=[ ⛔ The Module is NOT functioning. ] /g' "$TMP_MODULE_PROP"
-
-# Use flock to lock module.prop for exclusive access
-flock "$MODDIR/module.prop" -c "mount --bind '$TMP_MODULE_PROP' '$MAIN_MODULE_PROP'"
-
-# Remove the status directory
-rm -rf "$NVBASE/yt-revanced/.tmp/status"
-
+# Exit with success
 exit 0
