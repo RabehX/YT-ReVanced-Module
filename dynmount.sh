ae6bce15c61f9fc757ce3a6e6da35d4ebf602607
Рабах Аззам <64314390+RabahX@users.noreply.github.com>
2023-11-20 00:33:33 +0100

    YT-ReVanced-Module

--- dynmount.sh
+++ dynmount.sh
@@ -3,7 +3,7 @@
 MODDIR="${0%/*}"
 NVBASE="/data/adb"
 MAIN_MODULE_PROP="$NVBASE/modules/$(basename "$(dirname "$0")")/module.prop"
-YOUTUBE_PACKAGE_PATH=$(pm path com.google.android.youtube | head -1 | sed 's/^package://g')
+YOUTUBE_PACKAGE_PATH=$(pm path com.google.android.youtube | awk -F':' 'NR==1{print $2}')
 REVANCED_APK_PATH="$MODDIR/yt-revanced.apk"
 STAGE="$1"
 UID="$3"
@@ -11,29 +11,30 @@ PROC="$4"
 
 RUN_SCRIPT() {
     case "$STAGE" in
-    "prepareEnterMntNs")
-        prepareEnterMntNs
-        ;;
-    "EnterMntNs")
-        EnterMntNs
-        ;;
-    "OnSetUID")
-        exit 1
-        ;;
+    "prepareEnterMntNs") prepareEnterMntNs ;;
+    "EnterMntNs") EnterMntNs ;;
     esac
 }
 
 prepareEnterMntNs() {
-    [ "$API_VERSION" -lt 4 ] && exit 1
-
-    if [ "$PROC" == "com.google.android.youtube" ] || [ "$UID" -lt 10000 ] || [ "$PROC" == "com.android.systemui" ]; then
-        touch "$NVBASE/yt-revanced/.tmp/status"
-        DYNAMIC_MOUNT_STATUS=$(sed -E 's/^description=(\[.*][[:space:]]*)?/description=[ 😋 Dynamic mount is operational. ] /g' "$MODDIR/module.prop")
-        echo -n "$DYNAMIC_MOUNT_STATUS" >"$MAIN_MODULE_PROP"
-        exit 0
+    if [ "$API_VERSION" -lt 4 ] &&
+        ! { [ "$PROC" = "com.google.android.youtube" ] || [ "$UID" -lt 10000 ] || [ "$PROC" = "com.android.systemui" ]; }; then
+        exit 1
     fi
 
-    exit 1
+    touch "$NVBASE/yt-revanced/.tmp/status"
+    YTVERSION=$(dumpsys package com.google.android.youtube | awk -F= '/versionName/{print $2; exit}')
+    YRVERSION=$(awk -F= '/version/{print $2; exit}' "$MODDIR/module.prop" | sed 's/^v//')
+
+    if [ "$YTVERSION" != "$YRVERSION" ]; then
+        CHECK_VERSION_STATUS=$(sed -E 's/^description=(\[.*][[:space:]]*)?/description=[⛔ The module is deactivated due to an incompatible YouTube version] /g' "$MODDIR/module.prop")
+        echo -n "$CHECK_VERSION_STATUS" >"$MAIN_MODULE_PROP"
+        exit 1
+    fi
+    DYNAMIC_MOUNT_STATUS=$(sed -E 's/^description=(\[.*][[:space:]]*)?/description=[😋 The module is functioning] /g' "$MODDIR/module.prop")
+    echo -n "$DYNAMIC_MOUNT_STATUS" >"$MAIN_MODULE_PROP"
+    exit 0
+
 }
 
 EnterMntNs() {
@@ -43,7 +44,6 @@ EnterMntNs() {
     chmod 0755 "$REVANCED_APK_PATH"
     mount -o bind "$REVANCED_APK_PATH" "$YOUTUBE_PACKAGE_PATH"
     exit 1
-
 }
 
 RUN_SCRIPT
